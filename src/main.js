65ae12a31dd1f733a7cd4e8a1d1f55fb785e941c
aishasha <1026145889@qq.com>
2018-06-18 01:41:08 +0800

    音乐app脚手架

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
