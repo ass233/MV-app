7b51948e3d4528ee874e5b17bacc3cdf6c10c820
username <ass233.cm@163.com>
2018-06-18 15:44:11 +0800

    'header组件'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+import MHeader from 'components/m-header/m-header'
+export default{
+  components: {MHeader}
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
