318066905ccf8f47e66a864039340652b8a9d80f
aishasha <1026145889@qq.com>
2018-06-18 21:06:31 +0800

    添加导航和页面路由

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 export default{
-  components: {MHeader}
+  components: {MHeader,Tab}
 }
 </script>
 
