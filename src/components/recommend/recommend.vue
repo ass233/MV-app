215fcf9298aa799309db9a0f4c42e63572a58fdf
aishasha <1026145889@qq.com>
2018-06-23 19:22:28 +0800

    轮波图实现

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="recommend">
         <div class="recommend-content">
-            <div class="slider-wrapper"></div>
+            <div v-if="recommends.length" class="slider-wrapper">
+              <slider>
+                <div v-for="item in recommends">
+                  <a :href="item.linkUrl">
+                    <img :src="item.picUrl" alt="">
+                  </a>
+                </div>
+              </slider>
+            </div>
             <div class="recommend-list">
                 <h1 class="list-title">热门歌单推荐</h1>
                 <ul></ul>
@@ -11,9 +19,15 @@
 </template>
 
 <script>
+import Slider from '../../base/slider/slider.vue'
 import { getRecommend} from '../../api/recommend'
 import {ERR_OK} from '../../api/config'
 export default {
+  data(){
+    return{
+      recommends:[]
+    }
+  },
   created(){
    this._getRecommend();
  },
@@ -22,9 +36,13 @@ export default {
      getRecommend().then((res)=>{
        if(res.code == ERR_OK){
          console.log(res.data.slider);
+         this.recommends = res.data.slider;
        }
      })
    }
+ },
+ components:{
+   Slider
  }
 
 }
