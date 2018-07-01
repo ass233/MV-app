6f2b1418e12eb90ff1797b14a6cd676ef1a900b6
aishasha <1026145889@qq.com>
2018-07-01 17:45:25 +0800

    歌手页面

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,34 @@
 <template>
-    <div>歌手页面</div>
+    <div class="singer">歌手页面</div>
 </template>
 <script>
+import {getSingerList} from '../../api/singer'
+import {ERR_OK} from '../../api/config'
 export default {
-    
+    data(){
+        return{
+            singers:[]
+        }
+        
+    },
+    created(){
+        this._getSingerList();
+    },
+    methods:{
+        _getSingerList(){
+            getSingerList().then((res)=>{
+                if(res.code == ERR_OK ){
+                    this.singers = res.data.list
+                }
+            });
+        }
+    }
 }
 </script>
-<style lang="stylus" scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
