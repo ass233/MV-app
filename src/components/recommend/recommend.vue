42205a78fd307001cb97f5186e9796d9bca6cbc5
aishasha <1026145889@qq.com>
2018-07-01 13:08:27 +0800

    首页完毕

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,31 +1,45 @@
 <template>
     <div class="recommend">
-        <div class="recommend-content">
-            <div v-if="recommends.length" class="slider-wrapper">
-              <slider>
-                <div v-for="item in recommends">
-                  <a :href="item.linkUrl">
-                    <img :src="item.picUrl" alt="">
-                  </a>
-                </div>
-              </slider>
+        <scroll ref="scroll" class="recommend-content" :data="discList">
+            <div>
+                  <div v-if="recommends.length" class="slider-wrapper">
+                    <slider>
+                      <div v-for="item in recommends">
+                        <a :href="item.linkUrl">
+                          <img class="needsclick" @load = "loadImage" :src="item.picUrl" alt="">
+                        </a>
+                      </div>
+                    </slider>
+                  </div>
+                  <div class="recommend-list">
+                      <h1 class="list-title">热门歌单推荐</h1>
+                      <ul>
+                        <li v-for="item in discList" class="item">
+                            <div class="icon">
+                              <img width="60" height="60" v-lazy="item.imgurl">
+                            </div>
+                            <div class="text">
+                              <h2 class="name" v-html="item.creator.name"></h2>
+                              <p class="desc" v-html="item.dissname"></p>
+                            </div>
+                        </li>
+                      </ul>
+                  </div>
             </div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单推荐</h1>
-                <ul></ul>
-            </div>
-        </div>
+        </scroll>
     </div>
 </template>
 
 <script>
+import Scroll from '../../base/scroll/scroll'
 import Slider from '../../base/slider/slider.vue'
 import { getRecommend, getDiscList} from '../../api/recommend'
 import {ERR_OK} from '../../api/config'
 export default {
   data(){
     return{
-      recommends:[]
+      recommends:[],
+      discList:[]
     }
   },
   created(){
@@ -44,19 +58,25 @@ export default {
    _getDiscList(){
      getDiscList().then((res)=>{
        if(res.code == ERR_OK){
-         console.log(res.data.list);
+         this.discList = res.data.list;
        }
      })
+   },
+   loadImage(){
+     if(this.checkloaded){
+       this.checkloaded = true
+       this.$refs.scroll .refresh()
+     }
    }
  },
  components:{
-   Slider
+   Slider,
+   Scroll
  }
-
 }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+    @import "~common/stylus/variable"
   .recommend
     position: fixed
     width: 100%
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import Vuelazyload from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
 /* eslint-disable no-new */
+Vue.use(Vuelazyload,{
+  loading:require('./common/image/default.png')
+})
 new Vue({
   el: '#app',
   router,
