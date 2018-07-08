9937cbee9a2518d6c48a6774eee8dc6ed3b550e5
aishasha <1026145889@qq.com>
2018-07-08 19:09:55 +0800

    歌手页面

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -18,6 +18,10 @@ export default{
         data:{
             type:Array,
             default:null
+        },
+        listenScroll:{
+          type:Boolean,
+          default:false
         }
     },
     mounted(){
@@ -34,6 +38,12 @@ export default{
                 probeType:this.probeType,
                 click:this.click
             })
+            if(this.listenScroll){
+              let me = this
+              this.scroll.on('scroll',(pos)=>{
+                me.$emit('scroll',pos)
+              })
+            }
         },
         enable(){
             this.scroll && this.scroll.enable()
@@ -43,6 +53,12 @@ export default{
         },
         refresh(){
             this.scroll && this.scroll.refresh()
+        },
+        scrollTo(){
+          this.scroll && this.scroll.scrollTo.apply(this.scroll,arguments)
+        },
+        scrollToElement(){
+          this.scroll && this.scroll.scrollToElement.apply(this.scroll,arguments)
         }
     },
     watch:{
@@ -55,5 +71,5 @@ export default{
 }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    
+
 </style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="singer">歌手页面</div>
+    <div class="singer">
+      <list-view :data="singers"></list-view>
+    </div>
 </template>
 <script>
 import {getSingerList} from '../../api/singer'
 import {ERR_OK} from '../../api/config'
+import ListView from '../../base/listview/listview'
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 export default {
     data(){
         return{
             singers:[]
         }
-        
+
     },
     created(){
         this._getSingerList();
@@ -18,10 +23,59 @@ export default {
         _getSingerList(){
             getSingerList().then((res)=>{
                 if(res.code == ERR_OK ){
-                    this.singers = res.data.list
+                    this.singers = this._normalizeSinger(res.data.list)
+                    console.log(this._normalizeSinger(this.singers));
+                    //this._normalizeSinger(this.singers)
                 }
             });
+        },
+        _normalizeSinger(list){
+          let map ={
+            hot:{
+              title:HOT_NAME,
+              items:[]
+            }
+          }
+          list.forEach((item,index)=>{
+            if(index < HOT_SINGER_LEN){
+              map.hot.items.push({
+                id:item.Fsinger_mid,
+                name:item.Fsinger_name,
+                avatar:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+              })
+            }
+            const key = item.Findex
+            if(!map[key]){
+              map[key] = {
+                title:key,
+                items:[]
+              }
+            }
+            map[key].items.push({
+              id:item.Fsinger_mid,
+              name:item.Fsinger_name,
+              avatar:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+            })
+          })
+
+          let ret = []
+          let hot = []
+          for(let key in map) {
+            let val = map[key]
+              if(val.title.match(/[a-zA-Z]/)){
+                ret.push(val)
+              }else if(val.title === HOT_NAME){
+                hot.push(val)
+              }
+            }
+              ret.sort((a, b)=>{
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+              })
+              return hot.concat(ret)
         }
+    },
+    components:{
+      ListView
     }
 }
 </script>
