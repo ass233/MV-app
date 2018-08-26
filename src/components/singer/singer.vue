87e49dd3d33a09607b50487a792bcac4af11e6b4
ass233 <1026145889@qq.com>
2018-08-26 17:25:33 +0800

    '提交vuex'

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -8,6 +8,7 @@
 import {getSingerList} from '../../api/singer'
 import {ERR_OK} from '../../api/config'
 import ListView from '../../base/listview/listview'
+import {mapMutations} from 'vuex'
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
 export default {
@@ -25,6 +26,7 @@ export default {
         this.$router.push({
           path: `/singer/${singer.id}`
         })
+        this.setSinger(singer)
       },
         _getSingerList(){
             getSingerList().then((res)=>{
@@ -77,7 +79,10 @@ export default {
                 return a.title.charCodeAt(0) - b.title.charCodeAt(0)
               })
               return hot.concat(ret)
-        }
+        },
+      ...mapMutations({
+        setSinger:'SET_SINGER'
+      })
     },
     components:{
       ListView
