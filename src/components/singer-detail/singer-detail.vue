8cd9b554e98731546ae9ea960c3a53d4b6afd451
ass233 <1026145889@qq.com>
2018-10-28 18:31:52 +0800

    '获取歌手详情数据'

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -7,21 +7,36 @@
 
 <script type="text/ecmascript-6">
   import { mapGetters } from 'vuex'
-  export default {
-    computed:{
+  import { getSingerDetail } from '../../api/singer'
+  import { ERR_OK} from '../../api/config'
+
+export default {
+    computed: {
       ...mapGetters([
         'singer'
       ])
     },
-    created(){
-      console.log(this.singer)
+    created() {
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
+          }
+        })
+      }
     }
-  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
  @import "~common/stylus/variable"
-
   .singer-detail
     position :fixed
     z-index :100
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -12,81 +12,80 @@ import {mapMutations} from 'vuex'
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
 export default {
-    data(){
-        return{
-            singers:[]
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    selectSinger(singer) {
+      this.$router.push({
+        path: `/singer/${singer.id}`
+      })
+      this.setSinger(singer)
+    },
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code == ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+                    // this._normalizeSinger(this.singers)
         }
-
-    },
-    created(){
-        this._getSingerList();
-    },
-    methods:{
-      selectSinger(singer){
-        this.$router.push({
-          path: `/singer/${singer.id}`
-        })
-        this.setSinger(singer)
-      },
-        _getSingerList(){
-            getSingerList().then((res)=>{
-                if(res.code == ERR_OK ){
-                    this.singers = this._normalizeSinger(res.data.list)
-                    //this._normalizeSinger(this.singers)
-                }
-            });
-        },
-        _normalizeSinger(list){
-          let map ={
-            hot:{
-              title:HOT_NAME,
-              items:[]
-            }
-          }
-          list.forEach((item,index)=>{
-            if(index < HOT_SINGER_LEN){
-              map.hot.items.push({
-                id:item.Fsinger_mid,
-                name:item.Fsinger_name,
-                avatar:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
-              })
-            }
-            const key = item.Findex
-            if(!map[key]){
-              map[key] = {
-                title:key,
-                items:[]
-              }
-            }
-            map[key].items.push({
-              id:item.Fsinger_mid,
-              name:item.Fsinger_name,
-              avatar:`https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
-            })
-          })
-
-          let ret = []
-          let hot = []
-          for(let key in map) {
-            let val = map[key]
-            if (val.title.match(/[a-zA-Z]/)) {
-                ret.push(val)
-              }else if(val.title === HOT_NAME){
-                hot.push(val)
-              }
-            }
-              ret.sort((a, b)=>{
-                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
-              })
-              return hot.concat(ret)
-        },
-      ...mapMutations({
-        setSinger:'SET_SINGER'
       })
     },
-    components:{
-      ListView
-    }
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name,
+            avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+          })
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name,
+          avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+        })
+      })
+
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    },
+    ...mapMutations({
+      setSinger: 'SET_SINGER'
+    })
+  },
+  components: {
+    ListView
+  }
 }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
