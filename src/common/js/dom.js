d76611363b7e46e8b0572a9e44064cd97f2c7d3c
ass233 <1026145889@qq.com>
2018-11-21 01:20:07 +0800

    提交dom

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -13,12 +13,12 @@ export function addClass(el, className) {
   el.className = newClass.join(' ')
 }
 
-export function getData(el, name, val){
+export function getData(el, name, val) {
   const prefix = 'data-'
   name = prefix + name
-  if(val){
-    return el.setAttribute(name,val)
-  }else{
+  if (val) {
+    return el.setAttribute(name, val)
+  } else {
     return el.getAttribute(name)
   }
 }
