f2efc23ed06055a048f680e9f4f4740c134e63be
ass233 <1026145889@qq.com>
2018-12-16 15:21:52 +0800

    '横向滚动条开发结束'

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -30,7 +30,7 @@
           <div class="progress-wrapper">
             <span class="time time-l">{{ currentTime }}</span>
             <div class="progress-bar-wrapper">
-              <progress-bar></progress-bar>
+              <progress-bar :percent="percent" @percentChange="onProgressBarChange"></progress-bar>
             </div>
             <div class="time time-r"></div>
           </div>
@@ -197,6 +197,12 @@
         const minute = interval / 60 | 0
         const second = this._pad(interval % 60)
         return `${minute}:${second}`
+      },
+      onProgressBarChange(percent){
+        this.$refs.audio.currentTime = this.currentSong.duration * percent
+        if(!this.playing){
+          this.togglePlaying()
+        }
       },
       _pad(num, n = 2) {
         let len = num.toString().length
